5ad82ead7fe93bde2f1f1000c101c74cf65ed765
Raiyan Al Sultan <rraiyan77@gmail.com>
2023-12-24 15:05:45 +0600

    Inspecting dataset

--- stroke_prediction_ds.R
+++ stroke_prediction_ds.R
@@ -2,19 +2,16 @@
 stroke_ds <- read.csv("E:/Courses/AIUB Courses/9th Semester/Introduction to data science/Final Term/Strock Prediction Project/stroke_ds.csv")
 stroke_ds
 
-
 head(stroke_ds)
 tail(stroke_ds)
-
 str(stroke_ds)
+
 summary(stroke_ds)
-dim(stroke_ds)
-
-is.na(stroke_ds)
-
+colSums(is.na(stroke_ds))
 names(stroke_ds)
 
-colSums(is.na(stroke_ds))
+is.na(stroke_ds)
+dim(stroke_ds)
 
 
 
